580bd27da240ad42751a0d2f1da76a8e30825999
0x333333333 <69386697+0x333333333@users.noreply.github.com>
2023-07-11 23:19:50 +0800

    5 files deleted

--- assets/back.svelte
+++
@@ -1,3 +0,0 @@
-<svg width="60" height="60" viewBox="0 0 20 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M12.6665 31.8333L6.33317 25.4999L12.6665 19.1666" stroke="#5E1CEC" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
