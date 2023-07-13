1837b21ed28b27f751e1cabf82e752551ccc6c9d
BTX HK OTC <139428535+btx-cs@users.noreply.github.com>
2023-07-13 20:39:43 +0800

    唔使去VR機update了

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
 
-  // import Login from './Login.svelte';
   import LOGO from './assets/logo.svelte';
   import LOGOB from './assets/logo-g.svelte';
   import LOGOW from './assets/logo-w.svelte';
@@ -8,71 +8,74 @@
   import Price from './Price.svelte';
   import Back from './Background.svelte';
 
+  let interval;
 
+  onMount(() => {
+    interval = setInterval(() => {
+      // 在此处执行更新操作
+    }, 60000); // 1分钟 = 60000毫秒
+  });
+
+  // 在组件卸载时清除定时器
+  onDestroy(() => {
+    clearInterval(interval);
+  });
 </script>
 
 <main>
-
-<div class="header">
-<LOGO/>
-</div>
-<div class="headerw">
-<LOGOW/>
-</div>
-<div class="headerb">
-<LOGOB/>
-</div>
-
-
-<div class="body">
-<Back/>
-</div>
-<Quote/>
-<!-- <Price/> -->
-
-
+  <div class="header">
+    <LOGO/>
+  </div>
+  <div class="headerw">
+    <LOGOW/>
+  </div>
+  <div class="headerb">
+    <LOGOB/>
+  </div>
+  <div class="body">
+    <Back/>
+  </div>
+  <Quote/>
+  <!-- <Price/> -->
 </main>
 
 <style>
-
-
-
-.header {
-  position: fixed;
-  top: -60px;
-  left: -2%;
-  width: 100%;
-  padding: 10px;
-  z-index: 9999;
-  filter: drop-shadow(0 0 4em #0063be);
-}
-.headerw {
-  position: fixed;
-  top: -60px;
-  left: -2%;
-  width: 100%;
-  padding: 10px;
-  z-index: 9999;
-  animation: flashinga 9s infinite;
-}
-.headerb {
-  position: fixed;
-  top: -60px;
-  left: -2%;
-  width: 100%;
-  padding: 10px;
-  z-index: 9999;
-  animation: flashingb 9s infinite;
-  filter: drop-shadow(0 0 1em #ffffff);
-}
-.body {
-  opacity: 0.4;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  z-index: 9999;
-  filter: drop-shadow(0 0 4em #0063be);
-}
+  .header {
+    position: fixed;
+    top: -60px;
+    left: -2%;
+    width: 100%;
+    padding: 10px;
+    z-index: 9999;
+    filter: drop-shadow(0 0 4em #0063be);
+  }
+  .headerw {
+    position: fixed;
+    top: -60px;
+    left: -2%;
+    width: 100%;
+    padding: 10px;
+    z-index: 9999;
+    animation: flashinga 9s infinite;
+  }
+  .headerb {
+    position: fixed;
+    top: -60px;
+    left: -2%;
+    width: 100%;
+    padding: 10px;
+    z-index: 9999;
+    animation: flashingb 9s infinite;
+    filter: drop-shadow(0 0 1em #ffffff);
+  }
+  .body {
+    opacity: 0.4;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    z-index: 9999;
+    filter: drop-shadow(0 0 4em #0063be);
+  }
 </style>
