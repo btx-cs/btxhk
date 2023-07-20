5d827051d15a59e4d8cfaa25fdd96efa0bfb5ac9
BTX HK OTC <139428535+btx-cs@users.noreply.github.com>
2023-07-20 14:09:30 +0800

    20230720 price change

--- src/Quote.svelte
+++ src/Quote.svelte
@@ -1,6 +1,6 @@
 <script>
-  export let Sell_U = 7.79;
-  export let Buy_U = 7.735;
+  export let Sell_U = 7.78;
+  export let Buy_U = 7.73;
 
   import U from './assets/usdt.svelte';
   import B from './assets/btc.svelte';
