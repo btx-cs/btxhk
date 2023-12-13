fb4c54cf69e872b94a47129a0236be5a75f887a8
BTX HK OTC <139428535+btx-cs@users.noreply.github.com>
2023-12-13 12:26:14 +0800

    Update Quote.svelte

--- src/Quote.svelte
+++ src/Quote.svelte
@@ -1,6 +1,6 @@
 <script>
-  export let Sell_U = 7.805;
-  export let Buy_U = 7.755
+  export let Sell_U = 7.815;
+  export let Buy_U = 7.765
 
   import U from './assets/usdt.svelte';
   import B from './assets/btc.svelte';
@@ -43,7 +43,7 @@
   <h4 class="c1"><U/>USDT-HKD 即日匯率報價：</h4>
   <h1 class="c2"><h4 class="sell">{Buy_U}</h4> / <h4 class="buy">{Sell_U}</h4></h1>
   <h4 class="c3">（ 客戶賣出價 ） （ 客戶買入價 ）</h4>
-  <h4 class="c10">7.76 / 7.8 （大額交易價）</h4>
+  <h4 class="c10">7.77 / 7.81 （大額交易價）</h4>
 
   <h4 class="c4"><B/>BTC-HKD 即日匯率報價：</h4>
   <h1 class="c5"><h4 class="sell" on:click={()=>{BTC()}}>{buy_btc}</h4> / <h4 class="buy">{sell_btc}</h4></h1>
